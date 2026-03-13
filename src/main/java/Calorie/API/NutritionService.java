829c89278d7486c274e169469a6792ec950d832b
Veres Viktor Árpád <veresviktor02@gmail.com>
2026-03-13 14:01:44 +0100

    API kulcs tárolása az application.properties-ben rendszerváltozó helyett.

--- src/main/java/Calorie/API/NutritionService.java
+++ src/main/java/Calorie/API/NutritionService.java
@@ -1,5 +1,6 @@
 package Calorie.API;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,12 +12,14 @@ import java.net.URI;
 public class NutritionService {
     private static final String API_URL = "https://api.calorieninjas.com/v1/nutrition";
 
-    private static final String API_KEY = System.getProperty("api.key");
+    private final String apiKey;
 
     private final RestTemplate restTemplate;
 
-    public NutritionService(RestTemplate restTemplate) {
+    public NutritionService(RestTemplate restTemplate, @Value("${api.key}") String apiKey) {
         this.restTemplate = restTemplate;
+        this.apiKey = apiKey;
+        ;
     }
 
     public NutritionResponse getNutrition(String query) {
@@ -26,7 +29,7 @@ public class NutritionService {
                 .toUriString();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-Api-Key", API_KEY);
+        headers.set("X-Api-Key", apiKey);
 
         return restTemplate.exchange(
                 url,
